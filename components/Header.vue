30ae7f566efbae63982dd4906504227cc8678a70
aminoo10 <acminoo95@gmail.com>
2022-02-05 03:58:38 -0800

    updated api uri from v3 to v4

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,7 @@
 <template>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">AMLookup</b-navbar-brand> <!--tenative name until I come up with something cooler -->
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">AMLookup</b-navbar-brand>
+    <!--tenative name until I come up with something cooler -->
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
@@ -8,29 +9,50 @@
       <b-navbar-nav>
         <!-- I found out I dont have to use nuxt-link items for this so that's pretty cool -->
         <b-nav-item
-        v-for="navLink in navLinks"
-        v-bind:key="navLink.id"
-        :href="navLink.urlPath"
+          v-for="navLink in navLinks"
+          v-bind:key="navLink.id"
+          :href="navLink.urlPath"
         >
-        {{ navLink.name }}
+          {{ navLink.name }}
         </b-nav-item>
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav v-show="show" class="ml-auto">
         <b-nav-form>
-      <!-- I couldn't get the search input in the header to tie in well with the search from here, so I'm going to omitt it until I can figure out a good way of implementing it -->
-          <b-form-input v-on:keydown.enter="submit" v-model="query" size="sm" class="mr-sm-2 searchbar" placeholder="Search"></b-form-input>
+          <!-- I couldn't get the search input in the header to tie in well with the search from here, so I'm going to omit it until I can figure out a good way of implementing it -->
+          <b-form-input
+            v-on:keydown.enter="submit"
+            v-model="query"
+            size="sm"
+            class="mr-sm-2 searchbar"
+            placeholder="Search"> 
+            </b-form-input>
 
-            <b-button v-if="!disableButton" size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          <b-button
+            v-if="!disableButton"
+            size="sm"
+            class="my-2 my-sm-0"
+            type="submit"
+            >Search</b-button>
 
-            <b-button v-else disabled size="sm" class="my-2 my-sm-0" type="submit" title="Your search must have at least three (3) characters">Search</b-button>
-
-
+          <b-button
+            v-else
+            disabled
+            size="sm"
+            class="my-2 my-sm-0"
+            type="submit"
+            title="Your search must have at least three (3) characters">
+            Search</b-button>
         </b-nav-form>
-          <b-form-checkbox v-model="checked" name="check-button" switch class="ml-2">
-            <span id="checkbox-color"> Manga search</span>
-          </b-form-checkbox>
+        <b-form-checkbox
+          v-model="checked"
+          name="check-button"
+          switch
+          class="ml-2"
+        >
+          <span id="checkbox-color"> Manga search</span>
+        </b-form-checkbox>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -39,8 +61,7 @@
 <script>
 export default {
   name: 'my-header',
-  components: {
-  },
+  components: {},
   props: {
     show: Boolean,
     function: Function,
@@ -49,31 +70,28 @@ export default {
   data() {
     return {
       navLinks: [
-        {id: 1, urlPath: '/', name: 'Home'},
-        {id: 2, urlPath: '/recommendations', name: 'Recommendations'}
-      ]
+        { id: 1, urlPath: '/', name: 'Home' },
+        { id: 2, urlPath: '/recommendations', name: 'Recommendations' },
+      ],
     }
   },
-    computed: {
+  computed: {
     disableButton: {
       get() {
-        return (this.query.length < 3) ? true : false;
-      }
-    }
+        return this.query.length < 3 ? true : false
+      },
+    },
   },
   methods: {
-    submit () {
-      if (this.disableButton == false )  this.function;
-    }
-  }
-
+    submit() {
+      if (this.disableButton == false) this.function
+    },
+  },
 }
 </script>
 
 <style scoped lang="scss">
-
-  #checkbox-color {
-    color: white;
-  }
-
+#checkbox-color {
+  color: white;
+}
 </style>
